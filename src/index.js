62030aa2903fd3df36c4b4919a2bafa1937a8703
Ahmed <ahmedrather.s@gmail.com>
2021-07-13 13:19:02 -0400

    bootstrap setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./components/App";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
