ff40d67bc5a50f7f4e8c7c79560b4d1a4bb754f9
Ahmed <ahmedrather.s@gmail.com>
2021-08-05 16:26:08 -0400

    accordion working

--- src/components/EntriesList.js
+++ src/components/EntriesList.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllEntries } from "../redux/EntriesListSlice";
-
+import { Accordion, Card, Button } from "react-bootstrap";
 function EntriesList() {
   const entries = useSelector((state) => state.entriesList);
   const dispatch = useDispatch();
@@ -27,16 +27,130 @@ function EntriesList() {
     return <div>No results..</div>;
   }
 
+  let sortEntries = function (entryArray) {
+    let allEntries = {};
+    for (let element of entryArray) {
+      let entryDate = new Date(element.date);
+      let entryYear = entryDate.getFullYear();
+      let entryMonth = entryDate.getMonth();
+
+      if (!allEntries.hasOwnProperty(entryYear)) {
+        allEntries[entryYear] = {};
+        allEntries[entryYear][entryMonth] = [];
+        allEntries[entryYear][entryMonth].push(element);
+      } else {
+        if (!allEntries[entryYear].hasOwnProperty(entryMonth)) {
+          allEntries[entryYear][entryMonth] = [];
+          allEntries[entryYear][entryMonth].push(element);
+        } else {
+          allEntries[entryYear][entryMonth].push(element);
+        }
+      }
+    }
+
+    return allEntries;
+  };
+
   return (
-    <div>
-      <ul>
+    <>
+      {Object.keys(sortEntries(mappedEntries)).map((year, yearIndex) => {
+        return (
+          <Accordion defaultActiveKey="0">
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                  {year}
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  {Object.keys(sortEntries(mappedEntries)[year]).map(
+                    (month, monthIndex) => {
+                      return (
+                        <Accordion defaultActiveKey="0">
+                          <Card>
+                            <Card.Header>
+                              <Accordion.Toggle
+                                as={Button}
+                                variant="link"
+                                eventKey="0"
+                              >
+                                {month}
+                              </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="0">
+                              <Card.Body>
+                                {sortEntries(mappedEntries)[year][month].map(
+                                  (entry, entryIndex) => {
+                                    return (
+                                      <Card>
+                                        <Card.Header>
+                                          <Accordion.Toggle
+                                            as={Button}
+                                            variant="link"
+                                            eventKey="0"
+                                          >
+                                            {entry.date}
+                                          </Accordion.Toggle>
+                                        </Card.Header>
+                                        <Accordion.Collapse eventKey="0">
+                                          <Card.Body>
+                                            Hello! I'm the body
+                                          </Card.Body>
+                                        </Accordion.Collapse>
+                                      </Card>
+                                    );
+                                  }
+                                )}
+                              </Card.Body>
+                            </Accordion.Collapse>
+                          </Card>
+                        </Accordion>
+                      );
+                    }
+                  )}
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
+        );
+      })}
+    </>
+  );
+}
+
+/**
+ * <ul>
         {" "}
         {mappedEntries.map((e, i) => {
           return <li key={i}>{e.mood}</li>;
         })}{" "}
       </ul>
-    </div>
-  );
-}
+ */
+
+/**
+ *     <Accordion defaultActiveKey="0">
+      <Card>
+        <Card.Header>
+          <Accordion.Toggle as={Button} variant="link" eventKey="0">
+            Click me!
+          </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body>Hello! I'm the body</Card.Body>
+        </Accordion.Collapse>
+      </Card>
+      <Card>
+        <Card.Header>
+          <Accordion.Toggle as={Button} variant="link" eventKey="1">
+            Click me!
+          </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="1">
+          <Card.Body>Hello! I'm another body</Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
+ */
 
 export default EntriesList;
